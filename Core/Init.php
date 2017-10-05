93bf6ca70b11e42bca06dec9f142dda022b11cdc
megadevice83@gmail.com <megadevice83@gmail.com>
2017-10-05 22:23:53 +0300

    no message

--- Core/Init.php
+++ Core/Init.php
@@ -10,9 +10,35 @@ namespace Core;
 class Init{
     private $controller = APP.'\controllers\DefaultController';
     private $action = 'index';
-
+    private $args=[];
     public function __construct()
     {
-        echo "111";
+        $this->InitRoute()->CallAction();
+    }
+
+    public function InitRoute(){
+        $url = isset($_GET['_uri'])?$_GET['_uri']:'';
+        $segments=explode('/', $url);
+        if(!empty($segments[0])){
+            $this->action=$segments[0];
+            unset($segments[0]);
+            if(!empty($segments[1])){
+                $this->args=$segments;
+            }
+        }
+        return $this;
+    }
+
+    public function CallAction(){
+        if(class_exists($this->controller)){
+            $Controller=new $this->controller();
+            if(method_exists($Controller, $this->action)){
+                call_user_func_array([$Controller, $this->action], $this->args);
+            }else{
+                $Controller->error(404);
+            }
+        }else{
+            throw new \Exception("Controller [$this->controller] not found");
+        }
     }
 }
