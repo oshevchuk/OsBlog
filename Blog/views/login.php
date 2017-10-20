773c805894db2c6f9515961ff83f604825dfdcc2
megadevice83 <megadevice83@gmail.com>
2017-10-20 16:09:16 +0300

    12

---
+++ Blog/views/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oshevchuk
+ * Date: 20.10.2017
+ * Time: 16:08
+ */
--- Blog/views/register.php
+++ Blog/views/register.php
@@ -5,12 +5,13 @@ if($reginfo=="registered"){
 }else{
 ?>
 <p>Hello <?=\Core\Models\User::$login; ?> </p>
+    <p>reginster new user</p>
 <form action="/register" method="post" class="register-form">
     <label for="">Login</label>
     <input type="text" name="login" required>
     <label for="">password</label>
     <input type="password" name="password" required>
-    <input type="password" required>
+<!--    <input type="password" required>-->
     <input type="submit" value="register">
 </form>
 
